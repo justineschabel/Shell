5d526791d6956ea29902cd97ff863eef8b7f1918
jschabel <justine.schabel@gmail.com>
2019-04-08 19:28:35 -0700

    Fixing reading

--- sshell.c
+++ sshell.c
@@ -4,41 +4,122 @@
 #include <sys/wait.h>
 #include <string.h>
 
+
+void our_exit(){
+	fprintf(stderr, "Bye...\n");
+	exit(0);
+}
+
+
+void our_pwd(){
+	char *buffer = malloc(512); //ASK how big the path can be 
+	fprintf(stdout, "%s\n" , getcwd(buffer, 512)); //ASK: "usage: pwd [-L | -P]" exit [1]
+	return;
+}
+
+
+void our_cd(char* path){
+	fprintf(stdout, "Path: %s", path );
+	char *directory_cmd = malloc(1);
+	directory_cmd[0] = '.';
+	if(strcmp(path, directory_cmd)==0){
+		fprintf(stdout, "up one directory");
+	}
+	else{
+		// char *buffer = malloc(512); //ASK how big the path can be 
+		// char* current_path = getcwd(buffer, 512);
+
+		//chdir();
+	}
+	return;
+}
+
+
 /**
 arguments:
 	*potentially move command and args into a struct 
 	@command - The entire command line 
-	@program_mane - exclusively the program name/first argument
+	@program_name - exclusively the program name/first argument
 	@new_args - an array of every argument name including program_name
 purpose:
 	parse and store the entire command line
 **/
 
+// void parse_args(const char* command, char* program_name, char** new_args){
+// 	char *buffer = malloc(512);
+// 	int buffer_index = 0;
+// 	int new_args_index = 0;
+// 	int program_size = 0;
+
+// 	for(int i= 0; i < strlen(command); i++){
+// 		fprintf(stdout,"command: %s\n", command);
+// 		if(command[i] != ' '){ 
+// 			buffer[buffer_index] = command[i];
+// 			buffer_index++;
+
+// 			if(new_args_index == 0){ //first characters are the program name 
+// 				program_name[i] = command[i];
+// 				program_size++;
+// 			}
+// 		}
+// 		else{ //whitespace 
+// 			while(command[i] == ' '){ //eat up all whitespace
+// 				i++;
+// 				fprintf(stdout, "i in while: %d \n", i);
+// 			}
+// 			//memcpy(new_args[0], program_name, program_size);
+// 			fprintf(stdout, "i after while: %d, %c \n", i, command[i]);
+// 			new_args[new_args_index] = malloc(buffer_index);
+
+// 			fprintf(stdout, "Buffer: %s\n", buffer);
+
+// 			memcpy(new_args[new_args_index], buffer, buffer_index);
+// 			memset(buffer, 0, buffer_index);
+// 			buffer_index = 0;
+// 			new_args_index ++;
+// 			fprintf(stdout, "\n %d \n", i);
+// 		}
+// 	}
+// 	new_args[new_args_index] = NULL;
+// }
+
 void parse_args(const char* command, char* program_name, char** new_args){
 	char *buffer = malloc(512);
 	int buffer_index = 0;
 	int new_args_index = 0;
+	int program_size = 0;
 
 	for(int i= 0; i < strlen(command); i++){
 		if(command[i] != ' '){ 
 			buffer[buffer_index] = command[i];
 			buffer_index++;
+
+			if(new_args_index == 0){ //first characters are the program name 
+				program_name[i] = command[i];
+				program_size++;
+			}
 		}
 		else{ //whitespace 
 			while(command[i] == ' '){ //eat up all whitespace
 				i++;
 			}
-			if(new_args_index == 0){ //first characters are the program name 
-				memcpy(program_name, buffer, buffer_index);
-			}
 
-			new_args[new_args_index] = malloc(buffer_index);
-			memcpy(new_args[new_args_index], buffer, buffer_index);
+			fprintf(stdout, "Buffer: %s", buffer);
+
+			if (new_args_index == 0){
+				new_args[0] = malloc(program_size);
+				memcpy(new_args[0], program_name, program_size);
+			}
+			else{
+				new_args[new_args_index] = malloc(buffer_index);
+				memcpy(new_args[new_args_index], buffer, buffer_index);
+			}
 			memset(buffer, 0, buffer_index);
-			buffer_index = 0;
 			new_args_index ++;
+			buffer_index = 0;
 		}
 	}
+	new_args[new_args_index] = NULL;
 }
 
 
@@ -49,15 +130,17 @@ int main(int argc, char *argv[])
 	int status;
 	int pid;
 
+	//site skeleton code
 	while(1)
 	{
 		fprintf(stdout, "sshell$ ");
-		getline(&cmd, &cmdSize, stdin);
-
+		int length = getline(&cmd, &cmdSize, stdin);
+		cmd[length-1] = '\0';
 		char **args = malloc(17*sizeof(char*));
 		char *program_name = malloc(cmdSize);
 
-		parse_args(cmd, program_name ,args); 
+		parse_args(cmd, program_name ,args); 	
+
 
 		pid = fork();
 		if(pid == 0) //execute command as child
@@ -67,6 +150,15 @@ int main(int argc, char *argv[])
 		}
 		else if(pid > 0)
 		{
+			if(strcmp(program_name, "exit") == 0){
+				our_exit();
+			}
+			else if (strcmp(program_name, "pwd") == 0){
+				our_pwd();
+			}
+			else if(strcmp(program_name, "cd") == 0){
+				our_cd(args[1]);
+			}
 			waitpid(-1, &status, 0);
 			fprintf(stderr, "+ completed '%s': [%d]\n", cmd , WEXITSTATUS(status));
 		}
